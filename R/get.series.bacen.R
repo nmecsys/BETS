f220e71be528c0cefb0a7ae5136ec12abcb08c04
Fernando Teixeira <fernote7@gmail.com>
2017-09-05 15:21:10 -0300

    get.series.bacen uses httr only

--- R/get.series.bacen.R
+++ R/get.series.bacen.R
@@ -7,7 +7,6 @@
 #' @keywords bacen
 #' @author Fernando Teixeira \email{fernando.teixeira@fgv.br} and Jonatha Azevedo 
 #' \email{jonatha.costa@fgv.br}
-#' @import RCurl 
 #' @importFrom httr GET content
 
 get.series.bacen<- function(x, from = "", to = "",save = ""){
@@ -34,88 +33,33 @@ get.series.bacen<- function(x, from = "", to = "",save = ""){
   serie = mapply(paste0, "serie_", inputs, USE.NAMES = FALSE)
   
   
-  for (i in len){ 
-    
-    result = tryCatch({
-      
-      
-      RCurl::getURL(paste0('http://api.bcb.gov.br/dados/serie/bcdata.sgs.',
-                           inputs[i], 
-                           '/dados?formato=csv&dataInicial=', data_init, '&dataFinal=',
-                           data_end),
-                    ssl.verifyhost=FALSE, ssl.verifypeer=FALSE, .opts = list(timeout = 1, maxredirs = 2))
-    },
-    error = function(e) {
-      
-      return(RCurl::getURL(paste0('http://api.bcb.gov.br/dados/serie/bcdata.sgs.',
-                                  inputs[i], 
-                                  '/dados?formato=csv&dataInicial=', data_init,
-                                  '&dataFinal=',
-                                  data_end),
-                           ssl.verifyhost=FALSE, ssl.verifypeer=FALSE))
-      
-    })
-    
-    assign(serie[i], result) 
-  }
+ 
+ 
+ 
+
+ for(i in len){
+    texto=tryCatch({
+     k = paste0('http://api.bcb.gov.br/dados/serie/bcdata.sgs.',
+                inputs[i], 
+                '/dados?formato=csv&dataInicial=', data_init, '&dataFinal=',
+                data_end)
+     dados = httr::GET(k)
+     aux = httr::content(dados,'raw')
+     aux2= base::rawToChar(aux)
+     
+     DF <- data.frame(do.call(cbind, strsplit(aux2, "\r\n", fixed=TRUE)))
+     names(DF) <- "mist"
+     DF$mist   <- as.character(DF$mist)
+     DF$mist   <- gsub(x = DF$mist,pattern = '"',replacement = "")
+     DF$data   <- gsub(x = DF$mist,pattern = ";.*",replacement = "")
+     DF$valor  <- gsub(x = DF$mist,pattern = ".*;",replacement = "")
+     DF$valor  <- gsub(x = DF$valor,pattern = ",",replacement = ".")
+     DF <- DF[-1,-1]
+ })}
+ assign(serie[i], DF)
+ rm(DF, texto)
   
-  
- sinal = tryCatch({
-        for (i in len){
-            texto = utils::read.csv2(textConnection(eval(as.symbol(
-            serie[i]))), header=T)
-            texto$data = gsub(' .*$','', eval(texto$data))
-            assign(serie[i], texto)
-    
-  }},
-  error = function(e){
-      return("error")
-  })
- 
- 
- 
- if(sinal == "error"){
-     for(i in len){
-     texto=tryCatch({
-         k = paste0('http://api.bcb.gov.br/dados/serie/bcdata.sgs.',
-                    inputs[i], 
-                    '/dados?formato=csv&dataInicial=', data_init, '&dataFinal=',
-                    data_end)
-         dados = GET(k)
-         aux = content(dados,'raw')
-         aux2=rawToChar(aux)
-         
-         DF <- data.frame(do.call(cbind, strsplit(aux2, "\r\n", fixed=TRUE)))
-         names(DF) <-"mist"
-         DF$mist <- as.character(DF$mist)
-         DF$mist<- gsub(x = DF$mist,pattern = '"',replacement = "")
-         DF$data <- gsub(x = DF$mist,pattern = ";.*",replacement = "")
-         DF$valor <- gsub(x = DF$mist,pattern = ".*;",replacement = "")
-         DF$valor <- gsub(x = DF$valor,pattern = ",",replacement = ".")
-         DF <- DF[-1,-1]
-     })}
-     assign(serie[i], result)
-    }
-  
- if(sinal == "error"){
-     for (i in len){
-     texto$data = gsub(' .*$','', eval(texto$data))
-     assign(serie[i], texto)
-     }
- }
- 
- 
-  if(ncol(texto) == 1){
-    for (i in len){
-      texto = utils::read.csv(textConnection(eval(as.symbol(
-        serie[i]))), header=T)
-      texto$data = gsub(' .*$','', eval(texto$data))
-      assign(serie[i], texto)
-    }
-  }
-  
- rm(texto) 
- 
+
   lista = list()
   ls_df = ls()[grepl('data.frame', sapply(ls(), function(x) class(get(x))))]
   for ( obj in ls_df ) { lista[obj]=list(get(obj)) }
