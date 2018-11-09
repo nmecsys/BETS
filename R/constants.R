67ce57fb498dff68f45ad802d15e0cf015abd2f0
Jonatha Azevedo <johnmasterchip@gmail.com>
2018-11-09 16:17:28 -0200

    Create .MSG_NOT_SERVER_CONECTION constant variable

--- R/constants.R
+++ R/constants.R
@@ -5,7 +5,7 @@
 .MSG_LANG_NOT_AVAILABLE = "Language option is not avaiable. BETS reports are only available in english or portuguese."
 .MSG_OUT_NOT_AVAILABLE = "Output option is not avaiable. BETS charts can only be saved as .png ou .pdf"
 .MSG_BCB_DATA_OFFLINE = "The BCB data center is offline, please waite and try again"
-
+.MSG_NOT_SERVER_CONECTION = "BETS was unable to obtain information from the server about the time series of interest"
 
 # Wanining
 .WARN_SOFT = "Don't worry, this is not a critical problem. We are working on a solution."
